aea0c4a7fdca1963edca332c7a2ac942d7663771
evelynhelena <evelynhelena781@gmail.com>
2023-10-05 21:45:59 -0300

    iniciando storibook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,28 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$gray500',
-  borderRadius: '$md',
-  padding: '$4',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '#fff',
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Ola mundo</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
