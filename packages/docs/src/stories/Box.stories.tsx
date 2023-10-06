ec4c3e3bf66c395a0ac3fe00b3a67d98556b2a2e
evelynhelena <evelynhelena781@gmail.com>
2023-10-06 14:30:14 -0300

    WIP: Criando documentação

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <Text>Testando o elemento box</Text>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut et id aperiam. Porro qui, velit recusandae consequatur eveniet voluptatum illo at culpa vitae optio impedit excepturi eos suscipit. Cum, sit.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
